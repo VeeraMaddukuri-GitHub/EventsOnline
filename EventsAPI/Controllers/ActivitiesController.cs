b926a76ccfe32df342102fad80bcc420677d906f
Veera Maddukuri <veera.maddukuri@hotmail.com>
2021-03-29 12:15:59 +0100

    Initial project changes committing to git

---
+++ EventsAPI/Controllers/ActivitiesController.cs
@@ -0,0 +1,33 @@
+﻿using EventsPersistence;
+using EventsDomain;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EventsAPI.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _dataContext;
+        public ActivitiesController(DataContext context)
+        {
+            _dataContext = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _dataContext.Activities.ToListAsync();
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await _dataContext.Activities.FindAsync(id);
+        }
+
+    }
+}
---
+++ EventsAPI/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventsAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController: ControllerBase
+    {
+
+    }
+}
